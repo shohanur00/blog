2dbaaa9b2eddc75758115cc13ac49297625fa9e8
shohanur00 <shohanur.waltonbd@gmail.com>
2025-05-27 23:28:20 +0600

    category update

--- models/db.js
+++ models/db.js
@@ -14,7 +14,10 @@ const pool = new Pool({
     host: process.env.HOST,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    port: process.env.DBPORT
+    port: process.env.DBPORT,
+    ssl: {
+        rejectUnauthorized: false // Accept self-signed certs
+    }
 });
 
 // Graceful shutdown
